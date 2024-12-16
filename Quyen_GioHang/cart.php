e12f4f99203656e8d42969fbdb4ccea9de779b79
lewuyen <111169414+lewuyen@users.noreply.github.com>
2024-12-16 14:11:58 +0700

    Update cart.php dẫn css

--- Quyen_GioHang/cart.php
+++ Quyen_GioHang/cart.php
@@ -91,6 +91,7 @@ function format_currency($number) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Giỏ hàng</title>
     <link href="assets/css/style-cart.css" rel="stylesheet"/>
+    <link href="../style.css" rel="stylesheet"/>
     <link href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css" rel="stylesheet"/>
 </head>
 <body>
